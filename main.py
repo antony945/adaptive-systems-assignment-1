c5e32aa6dcd394efc1dabdf7c6f4a7ed2ffb6d84
Antonio Franzoso <antonio.franzoso@outlook.it>
2024-10-29 18:23:41 +0100

    Implemented proposal

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 from nltk.corpus import stopwords
 from nltk import PorterStemmer
 import datetime
+import numpy as np
 
 def get_all_values_in_column(df: pd.DataFrame, target_column: str):
     # Split the tags column by comma, expand it into a list, and flatten the resulting lists
@@ -27,7 +28,7 @@ def is_article_about_topics(df: pd.Series, target_topics: list[str]):
 def create_model(df: pd.DataFrame, main_column, porter, stoplist, isLDA: bool):    
     # Extract documents from df
     documents = df[main_column].dropna().to_list()
-    print(f"Vectorizing {len(documents)} documents...")
+    print(f"Vectorizing {len(documents)} documents based on '{main_column}'...")
 
     # remove common words and tokenize
     texts = [
@@ -76,10 +77,10 @@ def create_model(df: pd.DataFrame, main_column, porter, stoplist, isLDA: bool):
     print("Matrix similarities")
     print(matrix)
     print("======================================================\n")
-    return dictionary, model, vectors, matrix
+    return vectors, matrix
 
 # Implementation of the pseudocode above.
-def calculate_ratio_quality(df: pd.DataFrame, main_column, target_topics, porter, stoplist, dictionary, model, matrix, silent=True, topn=10):
+def calculate_ratio_quality(df: pd.DataFrame, target_topics, vectors, matrix, silent=True, topn=10):
     # Implement the following pseudocode to calculate the variable ratio_quality using the TFIDF vectors:
     total_goods = 0
     filtered_df = get_articles_by_topics(df, target_topics)
@@ -91,25 +92,13 @@ def calculate_ratio_quality(df: pd.DataFrame, main_column, target_topics, porter
     # For every article (a) on topic "Food and Drink":
     for index, row in filtered_df.iterrows():
         # Obtain the top-10 most similar articles (top-10) in Corpus to a
-        doc : str = row[main_column]
-        doc_s = [porter.stem(word) for word in doc.lower().split() if word not in stoplist]
-        vec_bow = dictionary.doc2bow(doc_s)
-        vec = model[vec_bow]
-
-        # print(doc)
-        # print("======")
-        # print(doc_s)
-        # print("======")
-        # print(vec_bow)
-        # print("======")
-        # print(vec)
-        # input("Continue..")
+        vec = vectors[index]
         
         # Calculate similarities between a and each doc of texts using tfidf/lda vectors and cosine
         sims = matrix[vec]
 
         # sort similarities in descending order
-        sims = sorted(enumerate(sims), key=lambda item: -item[1])[start_index:start_index+topn] # TODO: skip first one because it would be itself
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])[start_index:start_index+topn]
         
         if not silent:
             print()
@@ -135,7 +124,64 @@ def calculate_ratio_quality(df: pd.DataFrame, main_column, target_topics, porter
     ratio_quality = total_goods/(len(filtered_df)*10)
     return ratio_quality
 
-def run(main_column: str, target_topics_1, target_topics_2, isLDA: bool, silent=True, nrows=None):
+# Proposal
+def new_create_model(df: pd.DataFrame, column_a, column_b, porter, stoplist, isLDA: bool):
+    vectors_a, matrix_a = create_model(df, column_a, porter, stoplist, isLDA)
+    vectors_b, matrix_b = create_model(df, column_b, porter, stoplist, False) # don't use lda for computing tags-based similarity
+
+    return vectors_a, vectors_b, matrix_a, matrix_b
+
+def new_calculate_ratio_quality(df: pd.DataFrame, target_topics, vectors_a, vectors_b, matrix_a, matrix_b, silent=True, topn=10):
+    # Implement the following pseudocode to calculate the variable ratio_quality using the TFIDF vectors:
+    total_goods = 0
+    filtered_df = get_articles_by_topics(df, target_topics)
+    print(filtered_df[['headline', 'article_section']])
+    print("======================================================\n")
+
+    start_index = 0
+    alpha = 0.8
+
+    # For every article (a) on topic "Food and Drink":
+    for index, row in filtered_df.iterrows():
+        # Obtain the top-10 most similar articles (top-10) in Corpus to a
+        vec_a = vectors_a[index]
+        vec_b = vectors_b[index]
+        
+        # Calculate similarities between a and each doc of texts using tfidf/lda vectors and cosine
+        sims_a = matrix_a[vec_a]
+        sims_b = matrix_b[vec_b]
+
+        # Calculate the final combined similarity
+        sims = alpha * sims_a + (1 - alpha) * sims_b
+
+        # sort similarities in descending order
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])[start_index:start_index+topn]
+        
+        if not silent:
+            print()
+            print("Given the doc: " + row['headline'] + '\n')
+            print("The Similarities between this doc and the documents of the corpus are:")
+        for doc_position, doc_score in sims:
+            similar_article = df.iloc[doc_position]
+            
+            if not silent:
+                print(f"{doc_score} - {similar_article['article_section']} - {similar_article['headline']}")
+
+            # if doc_score == 1:
+            #     doc_s = [porter.stem(word) for word in similar_article[main_column].lower().split() if word not in stoplist]
+            #     print(doc_s)
+            #   input("Continue...")
+
+            # Count how many articles in top-10 are related to topic "Food and Drink" (goods)
+            if (is_article_about_topics(similar_article, target_topics)):
+                if not silent:
+                    print("-------SIMILAR------------")
+                total_goods = total_goods+1
+
+    ratio_quality = total_goods/(len(filtered_df)*10)
+    return ratio_quality
+
+def run(main_column: str, second_column: str, target_topics_1, target_topics_2, isLDA: bool, newMethod: bool, silent=True, nrows=None):
     init_t: datetime = datetime.datetime.now()
 
     # Read the Excel file
@@ -150,25 +196,26 @@ def run(main_column: str, target_topics_1, target_topics_2, isLDA: bool, silent=
     
     porter = PorterStemmer()
     stoplist = stopwords.words('english')
-    dictionary, model, vectors, matrix = create_model(df, main_column, porter, stoplist, isLDA)
-    
-    def convert(match):
-        return dictionary.id2token[int(match.group(0)[0:-1])]
 
-    # print()
-    # print("Vectors for documents (the positions with zeros are not shown):")
-    # for doc in vectors:
-    #     print(re.sub("[0-9]+,", convert, str(doc)))s
-
+    if newMethod:
+        vectors_a, vectors_b, matrix_a, matrix_b = new_create_model(df, main_column, second_column, porter, stoplist, isLDA)
+    else:
+        vectors, matrix = create_model(df, main_column, porter, stoplist, isLDA)
     end_creation_model_t: datetime = datetime.datetime.now()
     ###############################################################################################################################
 
-    ratio_quality1 = calculate_ratio_quality(df, main_column, target_topics_1, porter, stoplist, dictionary, model, matrix, silent)
+    if newMethod:
+        ratio_quality1 = new_calculate_ratio_quality(df, target_topics_1, vectors_a, vectors_b, matrix_a, matrix_b, silent)
+    else:
+        ratio_quality1 = calculate_ratio_quality(df, target_topics_1, vectors, matrix, silent)
     end_t1: datetime = datetime.datetime.now()
 
     ###############################################################################################################################
 
-    ratio_quality2 = calculate_ratio_quality(df, main_column, target_topics_2, porter, stoplist, dictionary, model, matrix, silent)
+    if newMethod:
+        ratio_quality2 = new_calculate_ratio_quality(df, target_topics_2, vectors_a, vectors_b, matrix_a, matrix_b, silent)
+    else:
+        ratio_quality2 = calculate_ratio_quality(df, target_topics_2, vectors, matrix, silent)
     end_t2: datetime = datetime.datetime.now()
 
     # get execution time
@@ -178,11 +225,11 @@ def run(main_column: str, target_topics_1, target_topics_2, isLDA: bool, silent=
 
     return ratio_quality1, ratio_quality2, elapsed_time_model_creation, elapsed_time_comparison1, elapsed_time_comparison2
 
-def run_experiment(main_column, target_topics_1, target_topics_2, silent, nrows):
+def run_experiment(main_column, second_column, target_topics_1, target_topics_2, newMethod, silent, nrows):
     # TF-IDF run
-    tfidf_ratio_quality_1, tfidf_ratio_quality_2, tfidf_elapsed_time_model_creation, tfidf_elapsed_time_comparison_1, tfidf_elapsed_time_comparison_2 = run(main_column, target_topics_1, target_topics_2, isLDA=False, silent=silent, nrows=nrows)
+    tfidf_ratio_quality_1, tfidf_ratio_quality_2, tfidf_elapsed_time_model_creation, tfidf_elapsed_time_comparison_1, tfidf_elapsed_time_comparison_2 = run(main_column, second_column, target_topics_1, target_topics_2, isLDA=False, newMethod=newMethod, silent=silent, nrows=nrows)
     # LDA run
-    lda_ratio_quality_1, lda_ratio_quality_2, lda_elapsed_time_model_creation, lda_elapsed_time_comparison_1, lda_elapsed_time_comparison_2 = run(main_column, target_topics_1, target_topics_2, isLDA=True, silent=silent, nrows=nrows)
+    lda_ratio_quality_1, lda_ratio_quality_2, lda_elapsed_time_model_creation, lda_elapsed_time_comparison_1, lda_elapsed_time_comparison_2 = run(main_column, second_column, target_topics_1, target_topics_2, isLDA=True, newMethod=newMethod, silent=silent, nrows=nrows)
 
     return [
         tfidf_ratio_quality_1, 
@@ -213,12 +260,15 @@ def print_result(result):
     print("====================================================================")
 
 if __name__ == '__main__':
-    main_column = "tags" # others are "description", "tags", "article_section"
+    main_column = "description" # values are "headline", "description", "tags", "article_section"
+    second_column = "tags" # values are "headline", "description", "tags", "article_section"
+    
     target_topics_a = ["food & drink", "food and drink"]
     target_topics_b = ["sports"]
     silent = True
+    newMethod = True
     nrows = None
 
-    result = run_experiment(main_column, target_topics_a, target_topics_b, silent, nrows)
+    result = run_experiment(main_column, second_column, target_topics_a, target_topics_b, newMethod, silent, nrows)
 
     print_result(result)
